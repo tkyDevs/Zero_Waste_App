9d268172ec56167b84d383c57c733368ceb5591c
KevinY <tkyDevs@gmail.com>
2025-01-10 01:49:59 -0330

    Added ejs warnings/messages

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 // -------------------------------------------- MONGOOSE
-const {User, connectMongoose} = require('./models/user');
+const {User, connectMongoose, isUsernameAvailable, isregisterUser} = require('./models/user');
 connectMongoose().catch(err => {
     console.log(`Error connecting to Mongoose: ${err}`);
 })
 
 // -------------------------------------------- EXPRESS
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('login.ejs');
+    let message = null;
+    res.render('login.ejs', {message});
 })
 
 app.get('/register', (req, res) => {
-    res.render('register.ejs');
+    let warning = null;
+    res.render('register.ejs', {warning});
+})
+app.post('/register', (req, res) => {
+    const {username, password, passwordCheck} = req.body;
+    if (password !== passwordCheck) {
+        res.render('register.ejs', {warning: "Passwords don't match! Please try again."});
+    } else if (!isUsernameAvailable) {
+        res.render('register.ejs', {warning: `Username ${username} is already taken!`});
+    } else {
+        res.render('login.ejs', {message: "User created successfully!"});
+    }
 })
 
 app.get('*', (req, res) => {
--- models/user.js
+++ models/user.js
@@ -30,4 +30,22 @@ async function connectMongoose() {
     console.log('Server connected to Mongoose!');
 }
 
-module.exports = {User, connectMongoose};
+async function isUsernameAvailable(username) {
+    const check = User.findOne({name: username});
+    if (Object.is(check, null)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function registerUser(username, password) {
+    if (isUsernameAvailable(username)) {
+        await User.create({name: username, password: password});
+        return `New User created! Username: ${username}`;
+    } else {
+        return `Username ${username} is taken!`
+    }
+}
+
+module.exports = {User, connectMongoose, isUsernameAvailable, registerUser};
